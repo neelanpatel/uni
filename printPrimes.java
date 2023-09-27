a0b4e7ceef3297824757d3217c8d6545324c1a1a
neelanpatel <146205445+neelanpatel@users.noreply.github.com>
2023-09-27 15:00:50 +0100

    Update printPrimes.java

--- printPrimes.java
+++ printPrimes.java
@@ -1,1 +1,17 @@
+static void printPrimes(int max){
+        for( int num=2; num <= max; num++){
+            boolean Prime = true; 
+            for(int f=2; f<=num/2; f++){
+                if(num%f==0){
+                    Prime=false;
+                }
+            }
+                if (Prime){
+                System.out.println(num);
+                }
 
+
+        
+
+        }
+    }
